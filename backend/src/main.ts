807451a230ea398b3d09b624605143655e5b01af
clark-john <mjohncl4rk@gmail.com>
2023-06-20 17:47:56 +0800

    change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 (async () => {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   console.log("Server listening at port " + port);
